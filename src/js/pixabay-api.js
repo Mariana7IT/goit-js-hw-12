127bb991f54f706d0a15b61421416b0bbd6f5c1d
Mariana7IT <153327568+Mariana7IT@users.noreply.github.com>
2024-05-28 23:26:07 +0300

    add_html

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,26 +1,9 @@
 'use strict';
 import axios from 'axios';
+
 const API_KEY = '23963114-6d0d5d874ae460d9125bacd21';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export function fetchImages(query) {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(
-    query
-  )}&image_type=photo&orientation=horizontal&safesearch=true`;
-
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`Error: ${response.statusText}`);
-      }
-      return response.json();
-    })
-    .then(data => data.hits)
-    .catch(error => {
-      console.log('Fetch error:', error);
-      return [];
-    });
-}
 let page = 1;
 
 export async function fetchImages(query, resetPage = false) {
